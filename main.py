1a4833415cdf73ad3677eea92c830e6a0e1ba8c0
kuziemsk <kuziemtogas@gmail.com>
2021-10-12 15:56:53 +0200

    First Commit

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 
-#test123
-def print_hi(name):
-        print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+import msvcrt as m
 
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def wait():
+    m.getch()
 
+os.system('cls')
 
+ans=True
+while ans:
+    print("""
+    Menu:
+    ---------------
+    
+    1. Exponential-E
+    2. NBI
+    3. Exit
+    """)
+    ans=input("Choose Project: ")
+    if ans=="1":
+      print("\nExponential-E was chosen")
+      ans=input("1. How many devices: ")
+      print("\nPress Enter...")
+      wait()
+      os.system('cls')
+      if ans == "1":
+        ans = input("Name of device: ")
+        wait()
+        os.system('cls')
+      elif ans == "2":
+        ans = input("Name of 1st device: ")
+        ans = input("Name of 2nd device: ")
+        print("\nPress Enter...")
+        wait()
+        os.system('cls')
+    elif ans=="2":
+      print("\ntest2")
+      print("\nPress Enter...")
+      wait()
+      os.system('cls')
+    elif ans=="3":
+      print("\nGoodbye")
+      ans = None
+    else:
+      print("\nNot Valid Choice Try again")
+      print("\nPress Enter...")
+      wait()
+      os.system('cls')
+      ans = True
